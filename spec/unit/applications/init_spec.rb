0ef7cee3317454034548780923222c0e74c0c145
ipcrm <mcadorette@gmail.com>
2017-01-21 23:24:35 -0500

    updating tests

--- spec/unit/applications/init_spec.rb
+++ spec/unit/applications/init_spec.rb
@@ -70,8 +70,68 @@ describe 'flask_app', :type => :application do
           it { should contain_package('python-pip').with( 'ensure' => 'present' ) }
         end
 
+    context "single node setup - customized" do
+      let(:title) { 'custom-deployment' }
+      let(:node) { 'test.puppet.com' }
 
+      let :params do
+        {
+          :app_name   => 'apptest',
+          :dist_file  => 'https://testrepo/puppet_flask_test.tar.gz',
+          :doc_root   => '/var/www/root',
+          :vhost_port => '8080',
+          :nodes => {
+            ref('Node', node) => [
+              ref('Flask_app::Webhead', 'custom-deployment'),
+            ]
+          }
+        }
       end
+
+        context 'with custom parameters' do
+          let(:pre_condition){'
+            class { "::apache":
+              default_vhost => false,
+            }
+          '}
+          it { should compile }
+          it { should contain_flask_app(title).with(
+            'app_name'  => 'apptest',
+            'dist_file' => 'https://testrepo/puppet_flask_test.tar.gz'
+          ) }
+
+          it { should contain_flask_app__webhead("custom-deployment").with(
+            'dist_file'     => 'https://testrepo/puppet_flask_test.tar.gz',
+            'local_archive' => 'apptest_archive.tar.gz',
+            'app_name'      => 'apptest',
+            'vhost_name'    => 'test.puppet.com',
+            'vhost_port'    => '8080',
+            'doc_root'      => '/var/www/root'
+          ) }
+
+          it { should contain_file('/var/www').with(
+            'ensure' => 'directory',
+          ) }
+
+          it { should contain_file('/var/www/root').with(
+           'ensure' => 'directory',
+          ) }
+
+          it { is_expected.to contain_file('/var/www/root/wsgi.py').with_content(/from apptest import apptest as application/) }
+
+          it { should contain_apache__vhost('test.puppet.com').with(
+            'port'               => '8080',
+            'docroot'            => '/var/www/root',
+            'wsgi_import_script' => '/var/www/root/wsgi.py',
+          ) }
+
+          it { should contain_exec('pip install apptest_archive.tar.gz') }
+          it { should contain_remote_file('apptest_archive.tar.gz') }
+          it { should contain_flask_app_http('custom-deployment') }
+          it { should contain_package('python-pip').with( 'ensure' => 'present' ) }
+        end
+      end
+     end
     end
   end
 end
