3583afb66b90f2e9e0ec7601255d358ee7118a65
ipcrm <mcadorette@gmail.com>
2017-01-21 21:42:38 -0500

    init acceptance tests

---
+++ spec/acceptance/flask_app.rb
@@ -0,0 +1,34 @@
+require 'spec_helper_acceptance'
+
+describe 'flask_app webhead define' do
+
+		context 'setup web' do
+      # Using puppet_apply as a helper
+      it 'should work idempotently with no errors' do
+        pp = <<-EOS
+        package {'wget': ensure => present, }
+        class {'::apache':
+          default_vhost => false,
+        }
+        flask_app::webhead {
+          $app_name => 'webui',
+        }
+        EOS
+
+        # Run it twice and test for idempotency
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes  => true)
+      end
+
+
+      describe port(80) do
+          it { should be_listening }
+      end
+
+      describe command('curl --silent http://localhost:80') do
+          its(:stdout) { should match /puppet/ }
+      end
+
+  end
+
+end
